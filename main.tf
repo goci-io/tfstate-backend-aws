33212cac3d634209d36fbce377c9be6cf5037b73
etwillbefine <tim.gatzemeier@gmx.de>
2019-08-16 17:06:16 +0200

    initial terraform 0.11 version

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.11.2"
+
+  # Do not reformat the following line!
+  backend          "s3"             {}
+}
+
+module "tfstate_backend" {
+  source        = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=tags/0.7.0"
+  namespace     = "${var.namespace}"
+  name          = "${var.name}"
+  attributes    = "${var.attributes}"
+  tags          = "${var.tags}"
+  region        = "${var.aws_region}"
+  force_destroy = "${var.force_destroy}"
+  stage         = "${var.stage}"
+  profile       = "${var.aws_profile}"
+}
---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Application or solution name (e.g. `app`)"
+  default     = "terraform"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = ["state"]
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
+variable "aws_region" {
+  type        = "string"
+  description = "AWS Region the S3 bucket should reside in"
+  default     = "eu-central-1"
+}
+
+variable "force_destroy" {
+  type        = "string"
+  description = "A boolean that indicates the S3 bucket can be destroyed even if it contains objects. These objects are not recoverable."
+  default     = "false"
+}
+
+variable "aws_profile" {
+  default     = ""
+  description = "AWS profile name as set in the shared credentials file"
+}
