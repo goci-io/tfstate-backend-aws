5a7641417c2417fea395ebe7bd8a9b7b05c23628
etwillbefine <tim.gatzemeier@gmx.de>
2019-08-17 17:40:13 +0200

    replace backend config for first init
    once initialized it is not possible to apply the terraform code
    without initializing again with a backend

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,15 +5,19 @@ set -e
 # Start from a clean slate
 rm -rf .terraform terraform.tfstate
 
+sed -Ei 's/^(\s+backend\s+)/#\1/' main.tf
+
 # Initialize terraform modules and providers
-terraform init -backend=false
+terraform init
 
 # Provision S3 bucket and dynamodb tables
 terraform apply -auto-approve
 
 export TF_BUCKET=$(terraform output tfstate_backend_s3_bucket_id)
 export TF_DYNAMODB_TABLE=$(terraform output tfstate_backend_dynamodb_table_id)
-export TF_BUCKET_REGION=${AWS_DEFAULT_REGION}
+export TF_BUCKET_REGION=${AWS_REGION}
+
+sed -Ei 's/^#(\s+backend\s+)/\1/' main.tf
 
 # Reinitialize terraform to import state to remote backend
 terraform init \
